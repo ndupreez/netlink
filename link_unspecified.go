b29a0740fe33239765c7a5826c62c06be2b7e656
Nick du Preez <nick@procwave.com>
2018-08-23 10:03:04 +1000

    Make mac builds work (part3)

--- link_unspecified.go
+++ link_unspecified.go
@@ -2,14 +2,7 @@
 
 package netlink
 
-import (
-    "github.com/vishvananda/netlink/nl"
-    "golang.org/x/sys/unix"
-)
-
 type LinkUpdate struct {
-    nl.IfInfomsg
-    Header unix.NlMsghdr
     Link
 }
 
